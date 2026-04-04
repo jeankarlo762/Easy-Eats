2c3beaaf19f7876cf6e914d69a4f1d0115fefae9
João Vitor <joaovitorbatista0104@gmail.com>
2026-04-04 12:11:42 -0300

    atualizado imports e botões para navegar entre as telas corretamente

--- src/app/Fila/component.fila.ts
+++ src/app/Fila/component.fila.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-fila',
   templateUrl: './component.fila.html',
-  styleUrls: ['./component.fila.scss']
+  styleUrls: ['./component.fila.scss'],
 })
 export class ComponentFila {
-
   activeOrders = [];
   readyOrders = [];
+  private router = inject(Router);
 
   updateOrderStatus(id: number) {}
 
+  protected acessarRota(rota: string) {
+    this.router.navigate([rota]);
+  }
 }
--- src/app/perfil-admin/perfilAdmin.ts
+++ src/app/perfil-admin/perfilAdmin.ts
@@ -2,17 +2,16 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'app-perfil-admin',
   imports: [RouterLink, RouterLinkActive],
-  templateUrl: './dashboard.html',
-  styleUrls: ['./dashboard.scss'],
+  templateUrl: './perfilAdmin.html',
+  styleUrl: './perfilAdmin.scss',
 })
-export class DashboardComponent {
+export class PerfilAdmin {
+  currentYear: any;
   private router = inject(Router);
 
   protected acessarRota(rota: string) {
     this.router.navigate([rota]);
   }
-
-  constructor() {}
 }
